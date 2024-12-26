d5d0e2e30440be35cb043aa3ea839838ab457589
Lee-Backend-Developer <eotmd1016@gmail.com>
2024-12-26 16:25:03 +0900

    GUI: 실행 잘 됨

---
+++ Main.java
@@ -0,0 +1,7 @@
+import ui.MainForm;
+
+public class Main {
+    public static void main(String[] args) {
+        new MainForm();
+    }
+}
---
+++ util/IpBrodcast.java
@@ -0,0 +1,19 @@
+package util;
+
+public interface IpBrodcast {
+    // 기예관 2층
+    String GIYEGWAN_2F = "172.20.32.255";
+
+    // 기예관 3층
+    String GIYEGWAN_3F = "172.20.33.255";
+
+    // 덕성관 2층
+    String DEOKSEONGGWAN_2F = "172.20.42.255";
+
+    // 덕성관 3층
+    String DEOKSEONGGWAN_3F = "172.20.43.255";
+
+    // 디자인센터 1층,2층
+    String DIJAINCENTER_1F_2F = "172.20.51.255";
+
+}
