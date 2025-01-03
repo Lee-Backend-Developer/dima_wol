6747ddd6d5e29bc72b7cfc083bc407fa1d304f84
Lee-Backend-Developer <eotmd1016@gmail.com>
2025-01-03 10:23:06 +0900

    추가기능 : 덕성관 203 컴퓨터 켜기

--- ui/deogseong/DeogseongTwoFloor.java
+++ ui/deogseong/DeogseongTwoFloor.java
@@ -1,6 +1,13 @@
 package ui.deogseong;
 
+import wol.IPObjectSocket;
+import wol.WolSend;
+import wol.ip.list.DeogseongComputerRoomMacAddr;
+import wol.ip.list.IPBrodcastList;
+
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class DeogseongTwoFloor extends JFrame {
     private JButton 이전Button;
@@ -17,5 +24,20 @@ public class DeogseongTwoFloor extends JFrame {
         setSize(500, 500);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
+        이전Button.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                new Deogseong();
+                setVisible(false);
+            }
+        });
+        a50203Button.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                for (String macAddress : DeogseongComputerRoomMacAddr.a50203) {
+                    WolSend.wolStart(new IPObjectSocket(macAddress, IPBrodcastList.DEOKSEONGGWAN_2F));
+                }
+            }
+        });
     }
 }
