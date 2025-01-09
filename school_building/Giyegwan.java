cc776246281933f05332abd15c9ceb9b5ac98b75
Lee-Backend-Developer <eotmd1016@gmail.com>
2025-01-09 16:42:32 +0900

    수정 : 기예관 이름, 수정하였음

--- school_building/Giyegwan.java
+++ school_building/Giyegwan.java
@@ -18,13 +18,13 @@ public class Giyegwan extends Building {
                 {},
                 // 2층
                 {
-                        new Room(2, "50203", a30225, IPBrodcastList.DEOKSEONGGWAN_2F.getBrodcast()),
+                        new Room(2, "30225", a30225, IPBrodcastList.GIYEGWAN_2F.getBrodcast()),
                 },
 
                 // 3층
                 {
-                        new Room(3, "50305", a30311, IPBrodcastList.DEOKSEONGGWAN_3F.getBrodcast()),
-                        new Room(3, "50310", a30323, IPBrodcastList.DEOKSEONGGWAN_3F.getBrodcast())
+                        new Room(3, "30311", a30311, IPBrodcastList.GIYEGWAN_3F.getBrodcast()),
+                        new Room(3, "30323", a30323, IPBrodcastList.GIYEGWAN_3F.getBrodcast())
                 },
                 // 4층
                 {}
